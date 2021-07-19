55684ca059fbd45d27fb8d2897d320673397ac3b
lomnom <zhaoxiong.ang@gmail.com>
2021-07-19 17:05:30 +0800

    Obamamium is now colored

--- examples/Obamamium.cpp
+++ examples/Obamamium.cpp
@@ -8,10 +8,12 @@ using std::string;
 
 // O B A M A M I U M
 int main(){
+	cout << esc::bgidcol(94);
 	cout << "╔═══════════╗" << endl;
 	cout << "║    " << esc::fgylw << "420" << esc::rstfg << "    ║" << endl;
 	cout << "║     " << esc::bld << "Ö" << esc::unblddim << "     ║" << endl;
 	cout << "║ " << esc::uline << "Obamamium" << esc::unuline << " ║" << endl;
 	cout << "║ " << esc::ital << "Metalloid" << esc::unital << " ║" << endl;
 	cout << "╚═══════════╝" << endl;
+	cout << esc::rstbg;
 }
