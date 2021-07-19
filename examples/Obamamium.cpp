ce643c464331f5cc369f8ea7e8d3efd12c0bd116
lomnom <zhaoxiong.ang@gmail.com>
2021-07-19 17:00:07 +0800

    O B A M A M I U M

---
+++ examples/Obamamium.cpp
@@ -0,0 +1,17 @@
+#include "../Escapes.hpp"
+#include <iostream>
+
+#define esc nc::Esc
+using std::cout;
+using std::endl;
+using std::string;
+
+// O B A M A M I U M
+int main(){
+	cout << "╔═══════════╗" << endl;
+	cout << "║    " << esc::fgylw << "420" << esc::rstfg << "    ║" << endl;
+	cout << "║     " << esc::bld << "Ö" << esc::unblddim << "     ║" << endl;
+	cout << "║ " << esc::uline << "Obamamium" << esc::unuline << " ║" << endl;
+	cout << "║ " << esc::ital << "Metalloid" << esc::unital << " ║" << endl;
+	cout << "╚═══════════╝" << endl;
+}
